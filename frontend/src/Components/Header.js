2800d3c669a3c390aa415539dd50b6e6fe6f8529
Danti <vedantsingh19dec@gmail.com>
2025-04-27 22:14:02 +0530

    Crafting the header for application

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header-bar">
+            <h1 style={{ marginLeft: "58px" }}>WalletWise</h1>
+        </div>
+    );
+};
+
+export default Header;
