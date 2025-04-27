f002d622f8206bd57ee9c8f16ae06b9995d032a7
Danti <vedantsingh19dec@gmail.com>
2025-04-27 22:42:14 +0530

    Routing to specific pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import 'devextreme/dist/css/dx.light.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Layout from "./Layout";
+import Transactions from "./Pages/Transactions";
+import ForgotPassword from "./Pages/ForgotPassword"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+
+                        <Route index element={
+                            <Transactions />
+                        } />
+
+                        <Route path="/register" element={
+                            <Register />
+                        } />
+
+                        <Route path="/login" element={
+                            <Login />
+                        } />
+
+                        <Route path="/resetPassword" element={
+                            <ForgotPassword />
+                        } />
+
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
