0e0f2ddc0ff82145794d8b640e6fc1f6f07532bd
Danti <vedantsingh19dec@gmail.com>
2025-04-27 20:14:24 +0530

    Sever initialized with dependencies and databased connected succesfully

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { connectDB } from "./DB/Database.js";
+
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+const port = process.env.PORT;
+
+connectDB();
+
+const allowedOrigins = [
+    "http://localhost:3000"
+];
+
+// Middleware
+app.use(express.json());
+app.use(
+    cors({
+        origin: allowedOrigins,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+);
+
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (request, response) => {
+    response.send("Hello World!");
+})
+
+app.use("/api/user", userRouter);
+app.use("/api/otp", otpRouter);
+app.use("/api/transaction", tranactionRouter);
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+})
